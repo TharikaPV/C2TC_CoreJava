472212169f4c8d510ec0e70c647f2a1e6f40d3c2
pvtha <pvtha@tharipv>
2025-09-27 20:37:02 +0530

    commit

---
+++ CoreJavaPrograms/src/Day3/Constructor/Customer.java
@@ -0,0 +1,57 @@
+package Day3.Constructor;
+
+public class Customer {
+
+    // Fields (instance variables)
+    private String customerName;
+    private int customerId;
+    private String customerCity;
+
+    // Default Constructor
+    public Customer() {
+        System.out.println("Default Constructor");
+    }
+
+    // Parameterized Constructor (with this keyword)
+    public Customer(String customerName, int customerId, String customerCity) {
+        this(); // calling default constructor
+        System.out.println("Parameterized Constructor");
+        this.customerName = customerName;
+        this.customerId = customerId;
+        this.customerCity = customerCity;
+    }
+
+    // Getters and Setters
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerCity() {
+        return customerCity;
+    }
+
+    public void setCustomerCity(String customerCity) {
+        this.customerCity = customerCity;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Customer [customerName=" + customerName + 
+               ", customerId=" + customerId + 
+               ", customerCity=" + customerCity + "]";
+    }
+}
+
---
+++ CoreJavaPrograms/src/Day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package Day3.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsDemo obj = new OopsDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+	}
+
+}
