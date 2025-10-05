b5ec4fc3740fe5d2f1ebf742525426d695c622e7
pvtha <pvtha@tharipv>
2025-10-05 06:14:35 +0530

    commit

--- CoreJavaPrograms/src/Day2/IfDemo.java
+++ CoreJavaPrograms/src/Day2/IfDemo.java
@@ -4,10 +4,12 @@ public class IfDemo {
 
 	public static void main(String[] args) {
 		int x = 3;
-		if (x != 3)
-			System.out.println("Value of x is not 3");
-		System.out.println("Value of x is 3"); 
+        if (x != 3)
+        	System.out.println("Value of x is not 3");
+        System.out.println("Value of x is 3"); 
 		
 	}
 
 }
+
+
