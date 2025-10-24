85a5c0feef420bf508e7f3af5cdb81abb52a858f
pvtha <pvtha@tharipv>
2025-10-24 19:47:50 +0530

    commit

--- CoreJavaPrograms/src/Day2/ForLoopDemo.java
+++ CoreJavaPrograms/src/Day2/ForLoopDemo.java
@@ -1,13 +1,9 @@
 package Day2;
 
-public class ForLoopDemo {
-
-	public static void main(String[] args) {
-		for(int i = 1; i<=1000 ; i++)
-		{	
-			System.out.print("Value of i:");
-			System.out.println(i);
-		}
+public class ForLoopDemo{
+   public static void main(String[] args) {
+	for(int i=1; i<10;i++) {
+		System.out.println("This is for loop demo\n"+ i);
 	}
-
+   }
 }
---
+++ CoreJavaPrograms/src/Day9/StringSubclass.java
@@ -0,0 +1,11 @@
+package Day9;
+//to demonstrate the error
+
+//can't inherit final classes
+public class StringSubClass extends String {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
