55404087feb4f61520473a9c1140b72c609ae650
pvtha <pvtha@tharipv>
2025-10-31 15:40:39 +0530

    commit

--- CoreJavaPrograms/src/Day9/StringSubclass.java
+++ CoreJavaPrograms/src/Day9/StringSubclass.java
@@ -2,10 +2,10 @@ package Day9;
 //to demonstrate the error
 
 //can't inherit final classes
-public class StringSubClass extends String {
+/*public class StringSubClass extends String {
 
 	public static void main(String[] args) {
 		
 
 	}
-}
+}*/
